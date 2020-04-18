99d235581128d988c1f5499b0050108c61cb4816
derekhus <derekhu@aaxischina.com>
2020-04-18 12:25:32 +0800

    update...

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import { Header, Footer } from "@aaxis/share-components";
 export default () => (
   <div>
     <Header />
-      <p>This is blog page in blog project.</p>
+      <p>This is blog page in <b>blog project</b>.</p>
       <div><a href='/'>Go Back</a></div>
       <Footer />
   </div>
