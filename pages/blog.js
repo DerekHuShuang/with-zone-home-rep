fd92039fa3c7c7e5da1ed67007cbacaad613f63d
derekhus <derekhu@aaxischina.com>
2020-04-20 18:26:25 +0800

    update...

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { Header, Footer } from "@aaxis/share-components";
-export default () => (
-  <div>
-    <Header />
-      <p>This is blog page in <b>blog project</b>.</p>
-      <div><a href='/'>Go Back</a></div>
-      <Footer />
-  </div>
-)
+import { Header, Footer, requestHelper } from "@aaxis/share-components";
+
+const wcsResourceConfig = { footerResourceId: '123213123123'}
+
+export default class Blog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            footerContent: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <p>This is blog page in <b>blog project</b>.</p>
+                <div><a href='/'>Go Back</a></div>
+                <Footer content={this.state.footerContent}/>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        const footerReq =  requestHelper.fetch({
+            uri: `/static/${wcsResourceConfig.footerResourceId}.html`,
+            method: 'get',
+        }).then((result) => {
+            this.setState({
+                footerContent: result.data
+            })
+        });
+    }
+}
+
